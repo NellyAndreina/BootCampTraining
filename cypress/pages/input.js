6bf89b28b3a111b85f86c42d377c89dff2dc697f
Nelly Mejias <nellymejias@MacBook-Air-2.local>
2024-03-13 17:03:29 -0300

    Descripción commit

--- cypress/pages/input.js
+++ cypress/pages/input.js
@@ -12,24 +12,20 @@ export class LinkInput {
   clickButton() {
     cy.get("#updatingButton").should("exist").click();
     cy.get("#updatingButton").click();
-    // cy.get('#result').should('contain', 'The action was successful');
   }
-  //class LinkInput {
-  // constructor() {
-  //    this.inputSelector = '#newButtonName';
-    //  this.buttonSelector = '#newButtonName';
-    //}
-    //enterTextInput(text) {
-      //cy.get(this.inputSelector).clear().type(text);
-    //}
-    //getTextInput() {
-      //return cy.get(this.inputSelector).invoke('val');
-    //}
-    //verifyTextButton(expectedText) {
-      //cy.get(this.buttonSelector).should('have.text', expectedText);
-    //}
-  //}
-  
-  //export default TestInputButtonPage;
+   constructor() {
+      this.inputSelector = '#newButtonName';
+      this.buttonSelector = '#newButtonName';
+    }
+    enterTextInput(text) {
+      cy.get(this.inputSelector).clear().type(text);
+    }
+    getTextInput() {
+      return cy.get(this.inputSelector).invoke('val');
+    }
+    verifyTextButton(expectedText) {
+      cy.get(this.buttonSelector).should('have.text', expectedText);
+    }
+
 }
 
